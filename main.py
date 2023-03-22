4895494cc3f3f8795f13b7df42ba7bb43ba5ad1a
LumieOwO <120320080+LumieOwO@users.noreply.github.com>
2023-03-22 21:58:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import socket
 from utils.initializehttpsocket import InitializeHttpSocket
 from threading import Thread
 
+
 def NUTS():
     print("nutsocket")
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     webpage = InitializeHttpSocket(HOST="localhost", PORT=80)
     Thread(target=webpage.add_endpoint(endpoint="/uwu",
-           html_file="OWO/ilovehate.html", METHOD=["POST","GET"],function_hook=NUTS)).start()
+           html_file="OWO/ilovehate.html", METHOD=["POST", "GET"], function_hook=NUTS)).start()
     Thread(target=webpage.add_endpoint(endpoint="/",
-            html_file="OWO/test.html",METHOD=["GET"])).start()
+                                       html_file="OWO/test.html", METHOD=["GET"])).start()
     Thread(target=webpage.run()).start()
--- utils/initializehttpsocket.py
+++ utils/initializehttpsocket.py
@@ -7,39 +7,40 @@ class InitializeHttpSocket:
                  HOST: str,
                  PORT: int,
                  ) -> None:
-        self.__HOST:str = HOST
-        self.__PORT:str = PORT
-        self.__endpoints:dict = {}
+        self.__HOST: str = HOST
+        self.__PORT: str = PORT
+        self.__endpoints: dict = {}
 
-    def add_endpoint(self, endpoint: str, html_file: str, METHOD=["GET"],function_hook=None) -> None:
-        self.__endpoints[endpoint] = [html_file, METHOD,function_hook]
+    def add_endpoint(self, endpoint: str, html_file: str, METHOD=["GET"], function_hook=None) -> None:
+        self.__endpoints[endpoint] = [html_file, METHOD, function_hook]
         return None
 
     def run(self) -> None:
-        self.__socket:socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket: socket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
         self.__socket.bind((self.__HOST, self.__PORT))
         print(f"uwuing in http://{self.__HOST}:{self.__PORT}")
         Thread(target=self.__listen_to_connection()).start()
         return None
 
     def __handle_request(self, req: str) -> str:
-        __parsed_req:str = req.split()
+        __parsed_req: str = req.split()
         for endpoint in self.__endpoints:
             if endpoint == __parsed_req[1] and __parsed_req[0] in self.__endpoints[endpoint][1]:
-                __html_file:str = self.__endpoints[endpoint][0]
+                __html_file: str = self.__endpoints[endpoint][0]
                 if self.__endpoints[endpoint][2] != None:
                     Thread(target=self.__endpoints[endpoint][2]()).start()
                 with open(__html_file) as htmlf:
-                    __html_file:str = htmlf.read()
-                __response:str = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{__html_file}"
+                    __html_file: str = htmlf.read()
+                __response: str = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{__html_file}"
                 return __response
-        __response:str = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n404 Not Found"
+        __response: str = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n404 Not Found"
         return __response
 
-    def __handle_connection(self, conn:socket.socket) -> None:
+    def __handle_connection(self, conn: socket.socket) -> None:
         with conn:
             request = conn.recv(1048576)
-            data:str = self.__handle_request(request.decode())
+            data: str = self.__handle_request(request.decode())
             conn.sendall(data.encode('utf-8'))
             conn.close()
         return None
@@ -47,5 +48,5 @@ class InitializeHttpSocket:
     def __listen_to_connection(self) -> None:
         while True:
             self.__socket.listen()
-            conn,addr = self.__socket.accept()
+            conn, addr = self.__socket.accept()
             Thread(target=self.__handle_connection, args=[conn]).start()
